2af9702ccb914ff56c96b565ede0fe7fb5d8d502
reckdave <126987291+reckdave@users.noreply.github.com>
2024-04-15 17:03:47 +0100

    Add files via upload

--- ModConfiguration/EXAMPLE.gd
+++ ModConfiguration/EXAMPLE.gd
@@ -1,6 +1,8 @@
 extends Node
 
 var config_name = 'Example'
+var config_author = 'Nothing'
+var config_mod_ver_min = '1.1'
 var config_info = {'test':'string'}
 var config_loaded = false
 var config_node
@@ -13,13 +15,18 @@ func ConfigHandler():
 			if get_parent().has_node('Config_Scene'):
 				var node = get_parent().get_node('Config_Scene')
 				config_loaded = true
-				config_node = node.MakeConfig(config_name,config_info) 
+				if node.VERSION == null or (node.VERSION > config_mod_ver_min):
+					OS.alert("OUTDATED ModConfiguration, See the logs for more ","ERROR")
+					print('DOWNLOAD THE LATEST ModConfiguration VERSION: \nhttps://github.com/reckdave/Mod-Configuration/releases/tag/release1.1')
+				config_node = node.MakeConfig(config_name,config_author,config_info) 
+				config_info = config_node.ConfigValues
 			yield(get_tree(),"idle_frame")
 	else:
-		OS.alert("MISSING MODCONFIG, PLEASE DOWNLOAD OR THE MOD WILL NOT WORK", "ERROR")
+		OS.alert("MISSING MODCONFIG, RESULTING TO DEFAULT VALUES", "ERROR")
+		config_loaded = true
 
 func _ready():
 	ConfigHandler()
 	while !config_loaded:
 		yield(get_tree(),"idle_frame")
-	print(config_node.Config_Values['test'])
+	print(config_info['test'])
--- ModConfiguration/MAIN.gd
+++ ModConfiguration/MAIN.gd
@@ -6,26 +6,32 @@ onready var CONFIG_NODE = preload("res://ModConfiguration/Scenes/Config_Node.tsc
 onready var CONFIG_LIST = $CONFIG_NODES
 onready var ANIMATION = $ANIMATION
 
-var VERSION = 'VER:1.1'
+var VERSION = '1.1'
 
 var File_EXT = '.json'
 var Config_Path = 'user://config'
+
+var ModConfig_Info = {'SHOW_APP':true}
+var ModConfig_Author = 'reckdave'
+var ModConfig_Name = 'ModConfig'
+
 # MISC
 func pr(string):
 	print('[CONFIG] ', string)
-func CheckVar(string : String):
-	match typeof(str2var(string)):
-		TYPE_INT:
-			pr('int')
-			return int(string)
-		TYPE_STRING:
-			pr('str')
-			if string.to_lower() == 'true' or string.to_lower() == 'false':
-				return str2var(string.to_lower())
-			return string
-		TYPE_BOOL:
-			pr('bool')
-			return str2var(string)
+func CheckVar(string):
+	return string
+	#match typeof(str2var(string)):
+		#TYPE_INT:
+			#pr('int')
+			#return int(string)
+		#TYPE_STRING:
+			#pr('str')
+			#if string.to_lower() == 'true' or string.to_lower() == 'false':
+				#return str2var(string.to_lower())
+			#return string
+		#TYPE_BOOL:
+			#pr('bool')
+			#return str2var(string)
 
 func _ready():
 	var dir = Directory.new()
@@ -33,20 +39,22 @@ func _ready():
 	if !dir.dir_exists('config'):
 		pr('CONFIG FOLDER DOESNT EXIST.. CREATING')
 		dir.make_dir('config')
-	pr('STARTED, ' + VERSION)
+	pr('STARTED, VER: ' + VERSION)
+	var ModConfig_Node = MakeConfig(ModConfig_Name,ModConfig_Author,ModConfig_Info)
+	ModConfig_Info = ModConfig_Node.ConfigValues
 	ANIMATION.play("LOAD")
-	yield(get_tree().create_timer(2,false),"timeout")
+	yield(get_tree().create_timer(5,false),"timeout")
 	ANIMATION.play("INVISIBLE")
 
-func MakeConfig(Config_Name : String,Info : Dictionary):
+func MakeConfig(Config_Name : String,Author : String,Info : Dictionary):
 	if Info == null or Info.empty():
 		pr(Config_Name + ' DICTIONARY DOESNT EXIST / IS EMPTY.. CANCELLED')
 		return
 	var Config_File = File.new()
 	var Config_Final = CONFIG_NODE.instance()
 	Config_Final.name = Config_Name
-	Config_Final.Config_Values = Info
-	
+	Config_Final.ConfigValues = Info
+	Config_Final.ModAuthor = Author
 	var FullDir = Config_Path + '/' + Config_Name + File_EXT
 	var Dir = Directory.new()
 	Dir.open(Config_Path)
@@ -66,7 +74,7 @@ func MakeConfig(Config_Name : String,Info : Dictionary):
 			return
 		var Contents = str2var(JSON_Content)
 		pr(Contents)
-		Config_Final.Config_Values = Contents
+		Config_Final.ConfigValues = Contents
 	CONFIG_LIST.add_child(Config_Final)
 	Config_File.close()
 	return Config_Final
@@ -85,23 +93,23 @@ func _process(delta):
 			APP_OPEN = false
 			for config_nodes in range(CONFIG_LIST.get_child_count()):
 				var node = CONFIG_LIST.get_child(config_nodes)
-				var Config_name = node.name
+				var Config_Name = node.name
 				var Config_Info = {}
-				var FullDir = Config_Path + '/' + Config_name + File_EXT
-				for config_values in range(node.Config_Values.size()):
-					var Config_Title = node.Config_Values.keys()[config_values]
-					var Config_Setting = LIST_BOX.get_node(Config_name + '_' + Config_Title)
+				var FullDir = Config_Path + '/' + Config_Name + File_EXT
+				for config_values in range(node.ConfigValues.size()):
+					var Config_Title = node.ConfigValues.keys()[config_values]
+					var Config_Setting = LIST_BOX.get_node(Config_Name + '_' + Config_Title)
 					var Config_Value = CheckVar(Config_Setting.config_value)
-					node.Config_Values[Config_Title] = Config_Value
+					node.ConfigValues[Config_Title] = Config_Value
 					pr(Config_Value)
 				var file = File.new()
 				file.open(FullDir,File.WRITE)
-				file.store_string(JSON.print(node.Config_Values,'\t'))
+				file.store_string(JSON.print(node.ConfigValues,'\t'))
 				file.close()
 			menu.get_node('ANIMATIONS').play('0')
 			yield(get_tree().create_timer(0.2,false),"timeout")
 			menu.queue_free()
-	if !PC_SCENE:
+	if !PC_SCENE and ModConfig_Info['SHOW_APP']:
 		if 'PC' in get_parent().get_parent().get_node('0').get_child(0).name:
 			PC_SCENE = true
 			var applic = get_parent().get_parent().get_node("0").get_child(0)
@@ -123,17 +131,18 @@ func _input(event):
 				for config_nodes in range(CONFIG_LIST.get_child_count()):
 					var node = CONFIG_LIST.get_child(config_nodes)
 					var Config_Header = CONFIG_SETTINGS.instance()
-					var Config_name = node.name
+					var Config_Name = node.name
+					var Config_Author = node.ModAuthor
 					Config_Header.editable = false
-					Config_Header.config_title = Config_name
+					Config_Header.config_title = Config_Name + ' by ' + Config_Author
 					LIST_BOX.add_child(Config_Header)
-					for config_values in range(node.Config_Values.size()):
+					for config_values in range(node.ConfigValues.size()):
 						var Config_Setting = CONFIG_SETTINGS.instance()
-						var Config_Title = node.Config_Values.keys()[config_values]
-						var Config_Value = node.Config_Values[Config_Title]
+						var Config_Title = node.ConfigValues.keys()[config_values]
+						var Config_Value = node.ConfigValues[Config_Title]
 						Config_Setting.config_title = Config_Title
 						Config_Setting.config_value = Config_Value
-						Config_Setting.name = Config_name + '_' + Config_Title
+						Config_Setting.name = Config_Name + '_' + Config_Title
 						LIST_BOX.add_child(Config_Setting)
 				applic.add_child(MENU)
 				MENU.get_node('ANIMATIONS').play('1')
