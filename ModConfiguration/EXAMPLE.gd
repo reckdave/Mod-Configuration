8229c2048c2becf57593e41978e88128fe541350
reckdave <126987291+reckdave@users.noreply.github.com>
2024-05-25 15:39:23 +0100

    Add files via upload

--- ModConfiguration/EXAMPLE.gd
+++ ModConfiguration/EXAMPLE.gd
@@ -2,7 +2,7 @@ extends Node
 
 var config_name = 'Example'
 var config_author = 'Nothing'
-var config_mod_ver_min = '1.1'
+var config_mod_ver_min = '1.1.3'
 var config_info = {'test':'string'}
 var config_loaded = false
 var config_node
@@ -15,9 +15,6 @@ func ConfigHandler():
 			if get_parent().has_node('Config_Scene'):
 				var node = get_parent().get_node('Config_Scene')
 				config_loaded = true
-				if node.VERSION == null or (node.VERSION < config_mod_ver_min):
-					OS.alert("OUTDATED ModConfiguration, See the logs for more ","ERROR")
-					print('DOWNLOAD THE LATEST ModConfiguration VERSION: \nhttps://github.com/reckdave/Mod-Configuration/releases/tag/release1.1')
 				config_node = node.MakeConfig(config_name,config_author,config_info) 
 				config_info = config_node.ConfigValues
 			yield(get_tree(),"idle_frame")
@@ -26,7 +23,5 @@ func ConfigHandler():
 		config_loaded = true
 
 func _ready():
-	ConfigHandler()
-	while !config_loaded:
-		yield(get_tree(),"idle_frame")
-	print(config_info['test'])
+	yield(ConfigHandler(),"completed")
+	print("finished")
--- ModConfiguration/MAIN.gd
+++ ModConfiguration/MAIN.gd
@@ -4,66 +4,62 @@ onready var CONFIG_MENU = preload("res://ModConfiguration/Scenes/Config_Menu.tsc
 onready var CONFIG_SETTINGS = preload("res://ModConfiguration/Scenes/Config_Setting.tscn")
 onready var CONFIG_NODE = preload("res://ModConfiguration/Scenes/Config_Node.tscn")
 onready var CONFIG_LIST = $CONFIG_NODES
-onready var HTTP = $HTTPRequest
+onready var HTTP = $VersionRequest
 
-signal X_PRESSED
-
-var VERSION = '1.1.3a'
-var URL = "https://raw.githubusercontent.com/reckdave/Mod-Configuration/main/ModConfiguration/VERSION.json"
+const VERSION = '1.1.4'
+const URL = "https://raw.githubusercontent.com/reckdave/Mod-Configuration/main/ModConfiguration/VERSION.json"
+const trusted_mods = ["KinitoFIXES","deadname-deleter","ModConfig","kinitonoboot"]
 
 var File_EXT = '.json' # The file extention of the file name
 var Config_Path = 'user://config' # File path of the config folder
 
-var ModConfig_Info : Dictionary = {'SHOW_APP':true}
-var ModConfig_Author : String = 'reckdave' # me!!!
-var ModConfig_Name : String = 'ModConfig'
+var ModConfig_Info := {'SHOW_APP':true, 'VERSION_CHECK':true}
+var ModConfig_Author := 'reckdave' # me!!!
+var ModConfig_Name := 'ModConfig'
 
-var Load_Time : float = 0.1
+var translation_contents
+
+var Load_Time := 0.1
 
 # MISC
 func pr(string):
 	print('[CONFIG] ', string)
-func CheckVar(string):
-	return string
-	# UNUSED BECAUSE IT WAS NOT NEEDED
-	#match typeof(str2var(string)):
-		#TYPE_INT:
-			#pr('int')
-			#return int(string)
-		#TYPE_STRING:
-			#pr('str')
-			#if string.to_lower() == 'true' or string.to_lower() == 'false':
-				#return str2var(string.to_lower())
-			#return string
-		#TYPE_BOOL:
-			#pr('bool')
-			#return str2var(string)
 
 func _ready():
 	var dir = Directory.new()
 	dir.open('user://') # Opens directory
+	
 	if !dir.dir_exists('config'): # Sees if the a folder called 'config' exists
-		pr('CONFIG FOLDER DOESNT EXIST.. CREATING')
 		dir.make_dir('config') # Creates the folder
-	pr('STARTED, VER: ' + VERSION) # Easier to help people because I can just ask their version number
+		pr('CONFIG FOLDER DOESNT EXIST.. CREATING')
+	
+	
+	pr('STARTED, VER: %s' % VERSION) # Easier to help people because I can just ask their version number
 	var ModConfig_Node = MakeConfig(ModConfig_Name,ModConfig_Author,ModConfig_Info)
 	ModConfig_Info = ModConfig_Node.ConfigValues
-	HTTP.request(URL)
+	if ModConfig_Info["VERSION_CHECK"]:
+		HTTP.request(URL)
 
-func MakeConfig(Config_Name : String = "NULL",Author : String = "NULL",Info : Dictionary = {}): # The real stuff happens here
+func MakeConfig(Config_Name := "NULL",Author := "NULL",Info := {}): # The real stuff happens here
 	if Info == null or Info.empty(): # Checks if the dictionary is empty or null
-		pr(Config_Name + ' DICTIONARY DOESNT EXIST / IS EMPTY.. CANCELLED')
+		pr('%s DICTIONARY DOESNT EXIST / IS EMPTY.. CANCELLED' % Config_Name)
 		return
+	
+	# GETS INFO / CREATES INFO
+	
 	var Config_File = File.new() # The actual file
 	var Config_Final = CONFIG_NODE.instance() # Clones the node
 	Config_Final.name = Config_Name
 	Config_Final.ConfigValues = Info # Store the values
 	Config_Final.ModAuthor = Author
-	var FullDir = Config_Path + '/' + Config_Name + File_EXT # Full thing example: 'user:config/Example.json'
+	var FullDir = Config_Path + '/' + Config_Name + File_EXT # Full thing example: 'user://config/Example.json'
 	var Dir = Directory.new()
 	Dir.open(Config_Path)
+	
+	# DIRECTORY CHECK
+	
 	if !Dir.file_exists(FullDir): # Checks if the file exists
-		pr('CREATING ' + Config_Name)
+		pr('CREATING %s' % Config_Name)
 		Config_File.open(FullDir,File.WRITE)
 		Config_File.store_string(JSON.print(Info,'\t')) # Stores the data
 	else: # Load the config file if it does
@@ -71,28 +67,52 @@ func MakeConfig(Config_Name : String = "NULL",Author : String = "NULL",Info : Di
 		Config_File.open(FullDir,File.READ_WRITE)
 		var JSON_Content = Config_File.get_as_text() # Reads the file
 		var Validated_JSON = validate_json(JSON_Content) # Checks if the file is o'okay
+		
+		# CHECKS IF ITS A VALID JSON
+		
 		if !Validated_JSON:
-			pr('VALID JSON: ' + Config_Name + File_EXT)
+			pr('VALID JSON: %s' % Config_Name + File_EXT)
 		else:
-			pr('INVALID JSON: ' + Config_Name + File_EXT)
+			pr('INVALID JSON: %s' % Config_Name + File_EXT)
 			return
 		var Contents : Dictionary = str2var(JSON_Content)
-		for key in range(Info.size()):
-			if Contents.keys()[key] == null: # CHECKS IF THE KEY DOESNT EXIST
-				pr('FOUND MISSING KEY, ADDING KEY..')
-				var title_key = Info.keys()[key]
-				Contents[title_key] = Info[title_key] # ADDS IT INTO THE LIST
+		
+		# CHECK FOR MISSING KEY
+		
+		for key in Info:
+			if key in Contents:
+				pr("found")
+			else:
+				pr(Info[key])
+				Contents[key] = Info[key]
+			
+		# STORES THE INFO
+		
 		Config_File.store_string(JSON.print(Contents,'\t'))
 		Config_Final.ConfigValues = Contents # Actually store the values
 		pr(Contents)
+	
+	# ADDS THE NODE / CONFIG
+	
 	CONFIG_LIST.add_child(Config_Final) # Add the node to the `CONFIG_NODES`
 	Config_File.close() # Close the file since we dont need it anymore
+	
+	# TRUSTED CHECK
+	
+	Config_Final.Trusted = false
+	for trusted_mod_num in range(trusted_mods.size()):
+		if Config_Name == trusted_mods[trusted_mod_num]:
+			pr(Config_Name)
+			Config_Final.Trusted = true
+			break
+	
 	return Config_Final # Give the node to the modder
 
 
 # APP HANDLER
 var CAN_OPEN = true
 var FINISHED_LOADING = false
+var replaced = false
 
 func _save_config():
 	var applic = get_parent().get_parent().get_node("0").get_child(0)
@@ -102,19 +122,17 @@ func _save_config():
 	for config_nodes in range(CONFIG_LIST.get_child_count()):
 				var node = CONFIG_LIST.get_child(config_nodes)
 				var Config_Name = node.name
-				#var Config_Info = {}
 				var FullDir = Config_Path + '/' + Config_Name + File_EXT
 				for config_values in range(node.ConfigValues.size()):
 					var Config_Title = node.ConfigValues.keys()[config_values]
 					var Config_Setting = LIST_BOX.get_node(Config_Name + '_' + Config_Title)
-					var Config_Value = CheckVar(Config_Setting.config_value)
+					var Config_Value = Config_Setting.config_value
 					node.ConfigValues[Config_Title] = Config_Value
 					pr(Config_Value)
 				var file = File.new()
 				file.open(FullDir,File.WRITE)
 				file.store_string(JSON.print(node.ConfigValues,'\t'))
 				file.close()
-
 func _process(delta):
 	if FINISHED_LOADING and !CAN_OPEN:
 		var applic = get_parent().get_parent().get_node("0").get_child(0)
@@ -127,7 +145,7 @@ func _process(delta):
 			yield(get_tree().create_timer(0.2,false),"timeout")
 			menu.queue_free()
 	if ModConfig_Info['SHOW_APP']:
-		if get_parent().get_parent().get_node("0") == null: return
+		if !get_parent().get_parent().has_node("0"): return
 		if get_parent().get_parent().get_node("0").get_child(0) == null: return
 		if "PC" in get_parent().get_parent().get_node('0').get_child(0).name:
 			#PC_SCENE = true
@@ -161,6 +179,7 @@ func _input(event):
 					var Config_Header_Anim = Config_Header.get_node("ANIMATION")
 					Config_Header.editable = false
 					Config_Header.config_title = Config_Name + ' by [wave]' + Config_Author
+					Config_Header.get_node("ACTIVE/TrustedImage").visible = node.Trusted
 					LIST_BOX.add_child(Config_Header)
 					Config_Header_Anim.play("0")
 					yield(get_tree().create_timer(Load_Time,false),"timeout")
@@ -175,13 +194,19 @@ func _input(event):
 						LIST_BOX.add_child(Config_Setting)
 						Config_Setting_Anim.play('0')
 						yield(get_tree().create_timer(Load_Time,false),"timeout")
+				var Space_NODE = Control.new()
+				Space_NODE.name = "SPACE"
+				LIST_BOX.add_child(Space_NODE)
 				FINISHED_LOADING = true
 
 # UPDATE CHECK
 
 func _request_completed(result, response_code, headers, body):
-	var data = str2var(body.get_string_from_utf8())
-	pr(data["VERSION"])
-	if VERSION < data["VERSION"]:
-		OS.alert("THIS IS AN OUTDATED VERSION OF MODCONFIG \nDOWNLOAD LATEST AT THE GITHUB \nSEE LOGS FOR MORE","MOD CONFIG")
-		pr("DOWNLOAD: \nhttps://github.com/reckdave/Mod-Configuration/releases")
+	if response_code == 200:
+		var data = str2var(body.get_string_from_utf8())
+		pr(data["VERSION"])
+		if VERSION < data["VERSION"]:
+			OS.alert("THIS IS AN OUTDATED VERSION OF MODCONFIG \nDOWNLOAD LATEST AT THE GITHUB","MOD CONFIG")
+			OS.shell_open("https://github.com/reckdave/Mod-Configuration/releases/tag/release%s" % data["VERSION"])
+	else:
+		pr('FAILED TO GET VERSION NUMBER: ' + response_code)
